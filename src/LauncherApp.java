33d50e37f375c7d593ff40b09781097b12e3dc80
Cássio Freitas <cassio.rsfreitas@gmail.com>
2020-11-10 18:39:23 +0000

    Prompt library added / AppLauncher as entry point

---
+++ src/LauncherApp.java
@@ -0,0 +1,8 @@
+import org.academiadecodigo.javabank.application.App;
+
+public class LauncherApp {
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+}
---
+++ src/org/academiadecodigo/javabank/application/App.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.javabank.application;
+
+import org.academiadecodigo.bootcamp.Prompt;
+
+public class App {
+
+    public void start() {
+
+        Prompt prompt = new Prompt(System.in, System.out);
+
+    }
+}
