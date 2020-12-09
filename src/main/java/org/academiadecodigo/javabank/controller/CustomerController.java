67c814e9d2da2dd70cef5ca4df5a36e4ab91fb72
cassioacademia <cassio.rsfreitas@gmail.com>
2020-12-09 15:29:55 +0000

    Working

--- src/main/java/org/academiadecodigo/javabank/controller/CustomerController.java
+++ src/main/java/org/academiadecodigo/javabank/controller/CustomerController.java
@@ -5,8 +5,10 @@ import org.academiadecodigo.javabank.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Controller responsible for rendering {@link Customer} related views
@@ -37,5 +39,25 @@ public class CustomerController {
     public String listCustomers(Model model) {
         model.addAttribute("customers", customerService.list());
         return "customer/list";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/details/{id}")
+    public String customerById(Model model, @PathVariable Integer id) {
+
+        //GET
+        Customer customer = customerService.get(id);
+        //INJECT
+        model.addAttribute("customer", customer);
+        //VIEW
+        return "customer/customer";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/delete")
+    public String deleteById(@RequestParam ("id") Integer id) {
+
+        //GET
+        customerService.delete(id);
+        //VIEW
+        return "redirect:/customer/list";
     }
 }
